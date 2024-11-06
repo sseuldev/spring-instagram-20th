19bd998e1de23998eb9490749e9f0341241772ee
Hanseul Lee <lhs010107@gmail.com>
2024-11-07 01:29:52 +0900

    :sparkles: feat: 토큰 재발급 로직 구현

--- src/main/java/com/ceos20/instagram_clone/domain/member/controller/AuthController.java
+++ src/main/java/com/ceos20/instagram_clone/domain/member/controller/AuthController.java
@@ -2,12 +2,16 @@ package com.ceos20.instagram_clone.domain.member.controller;
 
 import com.ceos20.instagram_clone.domain.member.dto.request.SignupRequestDto;
 import com.ceos20.instagram_clone.domain.member.dto.response.MemberResponseDto;
+import com.ceos20.instagram_clone.domain.member.dto.response.TokenResponseDto;
 import com.ceos20.instagram_clone.domain.member.service.AuthService;
 import com.ceos20.instagram_clone.domain.member.service.MemberService;
 import com.ceos20.instagram_clone.global.common.response.CommonResponse;
 import com.ceos20.instagram_clone.global.common.response.ResponseCode;
+import com.ceos20.instagram_clone.global.jwt.JWTUtil;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -19,6 +23,7 @@ import org.springframework.web.bind.annotation.*;
 public class AuthController {
 
     private final AuthService authService;
+    private final JWTUtil jwtUtil;
 
     @PostMapping("/signup")
     @Operation(summary = "회원가입", description = "회원가입 요청 API")
@@ -27,9 +32,24 @@ public class AuthController {
         return new CommonResponse<>(ResponseCode.SUCCESS, authService.signup(request));
     }
 
+    @PostMapping("/reissue")
+    @Operation(summary = "토큰 재발급", description = "토큰 재발급 요청 API")
+    public CommonResponse<TokenResponseDto> reissue(HttpServletRequest request, HttpServletResponse response) {
+
+        String refreshToken = authService.extractRefreshToken(request);
+        authService.validateRefreshToken(refreshToken);
+
+        String newAccessToken = authService.reissueAccessToken(refreshToken);
+
+        authService.setNewAccessToken(response, newAccessToken);
+
+        return new CommonResponse<>(ResponseCode.SUCCESS, new TokenResponseDto(newAccessToken, null));
+    }
+
     @GetMapping("/admin")
+    @Operation(summary = "관리자 권한", description = "어드민 페이지 API")
     public String adminP() {
 
-        return "admin Controller";
+        return "Admin Controller 입니다";
     }
 }
--- src/main/java/com/ceos20/instagram_clone/domain/member/service/AuthService.java
+++ src/main/java/com/ceos20/instagram_clone/domain/member/service/AuthService.java
@@ -2,16 +2,23 @@ package com.ceos20.instagram_clone.domain.member.service;
 
 import com.ceos20.instagram_clone.domain.member.dto.request.SignupRequestDto;
 import com.ceos20.instagram_clone.domain.member.dto.response.MemberResponseDto;
+import com.ceos20.instagram_clone.domain.member.dto.response.TokenResponseDto;
 import com.ceos20.instagram_clone.domain.member.entity.Member;
 import com.ceos20.instagram_clone.global.exception.BadRequestException;
+import com.ceos20.instagram_clone.global.jwt.JWTUtil;
 import com.ceos20.instagram_clone.global.repository.MemberRepository;
+import io.jsonwebtoken.ExpiredJwtException;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
 
-import static com.ceos20.instagram_clone.global.exception.ExceptionCode.DUPLICATED_ADMIN_EMAIL;
-import static com.ceos20.instagram_clone.global.exception.ExceptionCode.DUPLICATED_ADMIN_USERNAME;
+import static com.ceos20.instagram_clone.global.exception.ExceptionCode.*;
 
 @Service
 @Transactional(readOnly = true)
@@ -20,6 +27,7 @@ public class AuthService {
 
     private final MemberRepository memberRepository;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    private final JWTUtil jwtUtil;
 
     /**
      * 회원 가입
@@ -43,6 +51,54 @@ public class AuthService {
         memberRepository.save(newMember);
 
         return MemberResponseDto.from(newMember);
+    }
+
+    /**
+     * Refresh Token 추출
+     * **/
+    public String extractRefreshToken(HttpServletRequest request) {
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if (cookie.getName().equals("refresh")) {
+                    return cookie.getValue();
+                }
+            }
+        }
+        throw new BadRequestException(NOT_FOUND_REFRESH_TOKEN);
+    }
+
+    /**
+     * Refresh Token 검증
+     * **/
+    public void validateRefreshToken(String refreshToken) {
+
+        try {
+            jwtUtil.isExpired(refreshToken);
+        } catch (ExpiredJwtException e) {
+            throw new BadRequestException(EXPIRED_PERIOD_REFRESH_TOKEN);
+        }
+
+        String category = jwtUtil.getCategory(refreshToken);
+        if (!category.equals("refresh")) {
+            throw new BadRequestException(INVALID_REFRESH_TOKEN);
+        }
+    }
+
+    /**
+     * Access Token 재발급
+     * **/
+    public String reissueAccessToken(String refreshToken) {
+        String nickname = jwtUtil.getUsername(refreshToken);
+        String role = jwtUtil.getRole(refreshToken);
+        return jwtUtil.createJwt("access", nickname, role, 1000L * 60 * 60 * 2);
+    }
+
+    /**
+     * 재발급 된 Access Token 응답 헤더 설정
+     * **/
+    public void setNewAccessToken(HttpServletResponse response, String newAccessToken) {
+        response.setHeader("access", newAccessToken);
     }
 
 
